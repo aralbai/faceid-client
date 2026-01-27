de4fe400c99fa197e4cdafd1334296fc6aa0e65f
aralbai <xabibullayevmm@gmail.com>
2026-01-27 14:57:25 +0500

    ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <div className="bg-purple flex items-center justify-center h-full p-10">
       <div className="bg-gray w-[60%] rounded-2xl p-10 h-full">
         {/* TOP SECTION  */}
-        <div className="rounded-md flex justify-between h-[10%] gap-10 mb-10">
-          <div className="bg-white rounded-4xl flex-1 flex flex-col items-center justify-center">
+        <div className="rounded-md flex justify-between gap-10 mb-10">
+          <div className="bg-white rounded-4xl flex-1 flex flex-col items-center justify-center p-5">
             <p className="mb-2">All employes</p>
             <h1 className="font-bold">500</h1>
           </div>
@@ -35,11 +35,11 @@ export default function Home() {
         {/* CENTER SECTION  */}
         <div className="flex gap-10">
           <div className="w-[60%]">
-            <div className="bg-white border-10 border-purple rounded-3xl h-[400px]"></div>
+            <div className="bg-white border-10 border-purple rounded-3xl h-100"></div>
 
             <div className="bg-white rounded-3xl mt-5 flex justify-between items-center p-5 px-10">
               <div className="flex gap-5 items-center">
-                <div className="h-[100px] w-[100px] rounded-full border-3 border-purple"></div>
+                <div className="h-25 w-25 rounded-full border-3 border-purple"></div>
 
                 <div>
                   <h1 className="font-bold">Hohn.D</h1>
@@ -58,10 +58,10 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="w-[40%] bg-white rounded-2xl p-5 px-10 max-h-full">
+          <div className="w-[40%] h-140 bg-white rounded-2xl p-5 flex flex-col">
             <h1 className="text-center font-medium">Present list</h1>
 
-            <ul className=" overflow-auto">
+            <ul className="employeeList overflow-auto p-5">
               <li className="flex justify-between border-b border-b-gray">
                 <Link href="">D.John</Link>
                 <p>-10:45:33</p>
@@ -92,6 +92,90 @@ export default function Home() {
                 <p>-10:45:33</p>
               </li>
 
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
+              <li className="flex justify-between border-b border-b-gray">
+                <Link href="">D.John</Link>
+                <p>-10:45:33</p>
+              </li>
               <li className="flex justify-between border-b border-b-gray">
                 <Link href="">D.John</Link>
                 <p>-10:45:33</p>
